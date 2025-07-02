6c78e7988d8e69e89043c85658e3b81aca23fdc3
Davi Costa de Melo <davicosta01082008@gmail.com>
2025-07-02 09:54:04 -0300

    chore: remove the `window,location.origin` assignment

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,7 @@ export const server = hc<AppType>(process.env.NEXT_PUBLIC_API_BASE_URL!, {
     })
 
     if (response.status === 401) {
-      window.location.href = '/login'
+      // window.location.href = '/login'
     }
 
     return response
