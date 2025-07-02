f9c11792eb69face3e54e1509063752f078f8088
Davi Costa de Melo <davicosta01082008@gmail.com>
2025-07-02 10:40:52 -0300

    feat: add test axios response to hono fetch

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,18 @@
 import type { AppType } from '@corenotes/server'
+import axios from 'axios'
 import { hc } from 'hono/client'
 
 export const server = hc<AppType>(process.env.NEXT_PUBLIC_API_BASE_URL!, {
   fetch: async (input: RequestInfo | URL, init?: RequestInit) => {
     console.log(init)
+
+    const axiosResponse = await axios.get('https://corenotes-server-production.up.railway.app/notes', {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+      withCredentials: true,
+    })
+
+    console.log('@AXIOS', axiosResponse)
 
     const response = await fetch(input, {
       method: init?.method ?? 'GET',
