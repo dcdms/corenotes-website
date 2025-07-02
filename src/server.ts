e60920a7d014bd231f62f069585053e215f417a2
Davi Costa de Melo <davicosta01082008@gmail.com>
2025-07-02 10:47:36 -0300

    chore: remove debug code from `server.ts`

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,8 @@
 import type { AppType } from '@corenotes/server'
-import axios from 'axios'
 import { hc } from 'hono/client'
 
 export const server = hc<AppType>(process.env.NEXT_PUBLIC_API_BASE_URL!, {
   fetch: async (input: RequestInfo | URL, init?: RequestInit) => {
-    console.log(init)
-
-    const axiosResponse = await axios.get('https://corenotes-server-production.up.railway.app/notes', {
-      method: 'GET',
-      headers: { 'Content-Type': 'application/json' },
-      withCredentials: true,
-    })
-
-    console.log('@AXIOS', axiosResponse)
-
     const response = await fetch(input, {
       method: init?.method ?? 'GET',
       body: init?.body ?? null,
@@ -25,7 +14,7 @@ export const server = hc<AppType>(process.env.NEXT_PUBLIC_API_BASE_URL!, {
     })
 
     if (response.status === 401) {
-      // window.location.href = '/login'
+      window.location.href = '/login'
     }
 
     return response
